8b301195f193048ed6cf6d0c9983f7e1ad9a4c4b
Laitooo San <33745947+laitooo@users.noreply.github.com>
2020-11-11 21:39:06 +0200

    Update SplashScreenActivity.java

--- android/app/src/main/java/zxc/laitooo/warewolfonline/activities/SplashScreenActivity.java
+++ android/app/src/main/java/zxc/laitooo/warewolfonline/activities/SplashScreenActivity.java
@@ -11,17 +11,27 @@ import zxc.laitooo.warewolfonline.constants.Integeres;
 public class SplashScreenActivity extends AppCompatActivity {
 
     Handler handler = new Handler();
+    AppData data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+        data = new AppData(this);
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                if (data.getData().isLogged()){
+                    Intent intent = new Intent(this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Intent intent = new Intent(this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         }, Integeres.SPLASH_SCREEN_DELAY);
     }
